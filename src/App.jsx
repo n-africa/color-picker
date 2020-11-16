92d69db436a4b35797a5e7187f4ec520cbd4d89b
Naima Africa Francis <naima.africa@gmail.com>
2020-11-15 19:11:01 -0500

    HSL ranges added and updated css flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return <div>Hello, World!</div>
+  state={
+    hue: '50',
+    saturation: '8',
+    lightness:'20',
   }
+
+  updateHue= event => {
+    this.setState({
+      hue: event.target.value,
+    })
+  }
+//   state = {
+//     title: 'Color Picker',
+//     color: '',
+//   }
+// updateColor = event =>{
+//   this.setState({
+//     color: event.target.value,
+//   })
+// }
+  render() {
+    const currentColor= `hsl(${this.state.hue},${this.state.saturation},${this.state.lightness})`
+
+    return(
+//       <main>
+//         <h1>{this.state.title}</h1>
+//   <div style={ { backgroundColor: `${this.state.color}`  } } >
+//     <input type='range' onChange ={this.updateColor}/>
+
+// </div>  
+// </main>
+<div>
+  <h1>Pick A Color, Any Color</h1>
+
+<div style={ { backgroundColor: currentColor  } } ></div>
+
+<section>
+
+Hue: <input type= "range" min="0" max="360" value={this.state.hue} onChange={this.updateHue}/>
+
+Saturation: <input type= "range" min="0" max="100" value={this.state.saturation} />
+
+Lightness: <input type= "range" min="0" max="100" value={this.state.lightness} />
+
+</section>
+
+</div>
+)
+}
 }
 
 export default App
